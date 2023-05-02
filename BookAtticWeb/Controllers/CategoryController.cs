d5cc879fcadcc5a06bb3ad57ca7c6083ad8e9490
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-05-02 16:37:42 -0300

    Server Side Validation

--- BookAtticWeb/Controllers/CategoryController.cs
+++ BookAtticWeb/Controllers/CategoryController.cs
@@ -30,9 +30,13 @@ namespace BookAtticWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
 
     }
