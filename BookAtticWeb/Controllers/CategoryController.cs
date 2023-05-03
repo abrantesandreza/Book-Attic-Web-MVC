f1054cbdea26371abff9ae6ef4b6ab6f0a7999f2
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-05-03 10:46:36 -0300

    TempData

--- BookAtticWeb/Controllers/CategoryController.cs
+++ BookAtticWeb/Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace BookAtticWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -75,6 +76,7 @@ namespace BookAtticWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -111,6 +113,7 @@ namespace BookAtticWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
